185121e83001e423967ccf9e2aaa218920d316fc
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-07-14 23:30:24 +0530

    roboshop - ansible

---
+++ vars/nodjes.groovy
@@ -0,0 +1,56 @@
+def call() {
+    pipeline {
+
+        agent {
+            node {
+                label 'workstation'
+            }
+        }
+
+        options {
+            ansiColor('xterm')
+        }
+        parameters {
+            choice(name: 'env', choices: ['dev', 'prod'], description: 'Pick environment')
+            choice(name: 'action', choices: ['apply', 'destroy'], description: 'Pick environment')
+        }
+
+        stages {
+
+
+            stage('Code Quality') {
+                steps {
+                    sh 'echo Code Quality'
+                   // sh 'ls -l'
+                   // sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.80.236:9000 -Dsonar.login=admin -Dsonar.password=admin123'
+                }
+            }
+
+            stage('Unit Test Cases') {
+                steps {
+                    sh 'echo Unit tests'
+                }
+            }
+
+            stage('CheckMarx SAST Scan') {
+                steps {
+                    sh 'echo Checkmarx Scan'
+                }
+            }
+
+            stage('CheckMarx SCA Scan') {
+                steps {
+                    sh 'echo Checkmarx SCA Scan'
+                }
+            }
+
+        }
+
+        post {
+            always {
+                cleanWs()
+            }
+        }
+
+    }
+}

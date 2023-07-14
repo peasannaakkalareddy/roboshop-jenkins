38403c20113d6fc57ac044ada3b6660c0d6729c6
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-07-14 22:05:38 +0530

    roboshop - ansible

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -7,6 +7,9 @@ def call() {
             }
         }
 
+        options {
+            ansiColor('xterm')
+        }
         parameters {
             choice(name: 'env', choices: ['dev', 'prod'], description: 'Pick environment')
         }

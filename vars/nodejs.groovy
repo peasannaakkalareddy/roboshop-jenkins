db07ddad253c873a40a0c6a55d4a4c43e65babfc
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-07-15 12:08:12 +0530

    roboshop - ansible

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -44,6 +44,13 @@ def call() {
                 }
             }
 
+            stage('Release Application') {
+                steps {
+                    sh 'env'
+                    sh 'echo Release Application'
+                }
+            }
+
         }
 
         post {

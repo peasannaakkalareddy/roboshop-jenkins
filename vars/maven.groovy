4b265017537715b53ca3980b2f91074cb195cdac
prasanna kumar <prasannaakkalareddy@gmail.com>
2023-07-15 14:03:00 +0530

    roboshop - ansible

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -47,6 +47,22 @@ def call() {
                 }
             }
 
+            stage('Release Application') {
+                when {
+                    expression {
+                        env.TAG_NAME ==~ ".*"
+                    }
+                }
+                steps {
+                    sh 'mvn package ; cp target/${component}-1.0.jar ${component}.jar'
+                    sh 'echo $TAG_NAME >VERSION'
+                    sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION'
+                    sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.87.83:8081/repository/cart/${component}-${TAG_NAME}.zip'
+
+                    sh 'echo Release Application'
+                }
+            }
+
 
         }
 
--- vars/python.groovy
+++ vars/python.groovy
@@ -40,6 +40,22 @@ def call() {
                 }
             }
 
+            stage('Release Application') {
+                when {
+                    expression {
+                        env.TAG_NAME ==~ ".*"
+                    }
+                }
+                steps {
+                    sh 'echo $TAG_NAME >VERSION'
+                    sh 'zip -r ${component}-${TAG_NAME}.zip *.ini *.py *.txt VERSION'
+                    sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.87.83:8081/repository/cart/${component}-${TAG_NAME}.zip'
+
+                    sh 'echo Release Application'
+                }
+            }
+
+
 
         }
 
